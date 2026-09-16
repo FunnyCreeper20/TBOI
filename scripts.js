d42019b4ed470848ed7ffc6a0893978ed3dd986d
FunnyCreeper20 <logan.d.chandra@gmail.com>
2026-09-15 22:41:06 -0700

    Remove keydown event listener for search input

--- scripts.js
+++ scripts.js
@@ -1,8 +1,2 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Keep gameplay keys from being intercepted by the search field.
-  document.addEventListener('keydown', function(e) {
-    if (document.activeElement !== document.querySelector('.search-input')) {
-      e.stopImmediatePropagation();
-    }
-  }, true);
 });
